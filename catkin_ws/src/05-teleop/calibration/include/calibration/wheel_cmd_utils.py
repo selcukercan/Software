4117dd0095667b51ad8d14bb5d3d145fda75e348
Selcuk Ercan <ercans@ethz.ch>
2018-12-05 16:20:55 +0100

    input class + ui class

--- catkin_ws/src/05-teleop/calibration/include/calibration/wheel_cmd_utils.py
+++ catkin_ws/src/05-teleop/calibration/include/calibration/wheel_cmd_utils.py
@@ -1,22 +1,31 @@
 from abc import ABCMeta, abstractmethod
+import rospy
+from math import cos
 
-class AbstractClassExample(ABCMeta):
+class BaseExperimentClass(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
     @abstractmethod
     def generate_input(self, param_dict):
         """gets a dictionary containing the parameter values that are required to generate the input sequence for the particular input genre"""
         return
 
-class RampUp(AbstractClassExample):
+class RampUp(BaseExperimentClass):
 
-    def __init__(self, experiment_name, parameter_dict):
-        self.name = experiment_name
-        self.parameter_dict = parameter_dict
+    def __init__(self):
+        rospy.loginfo("initialized RampUp experiment with the default values for parameters")
+        self.name = "ramp_up"
+        self.parameter_dict = {'Nstep': 180, 'vFin': 0.5}
 
-        self.generate_input(parameter_dict)
+    def generate_input(self, parameter_dict):
+        parameter_dict = self.parameter_dict
 
-    def generate(self, parameter_dict):
         input_sequence = {'left_wheel': [], 'right_wheel': []}
-        rospy.loginfo("[{}-generate] generating input sequence of type {} with parameters {}".format("Sine", str(parameter_dict)))
+        rospy.loginfo("[generate_input] generating input sequence of type {} with parameters {}".format("RampUp", str(parameter_dict)))
 
         for n in range(1, parameter_dict['Nstep'] + 1):
             v = parameter_dict['vFin']/parameter_dict['Nstep'] * n
@@ -25,11 +34,67 @@ class RampUp(AbstractClassExample):
         input_sequence['left_wheel'].append(0)
         input_sequence['right_wheel'].append(0)
 
-    def SinCalib(self):
-        rospy.loginfo("Sin calibration starts")
+        return input_sequence
 
-        for t in range(0,self.duration,10):
-            self.sendCommand(self.k1+self.k2*cos(self.omega*t),self.k1-self.k2*cos(self.omega*t))
-            rospy.sleep(1/self.frequency)
 
-        self.sendCommand(0,0)
+class Sine(BaseExperimentClass):
+
+    def __init__(self):
+        rospy.loginfo("initialized Sine experiment with the default values for parameters")
+        self.name = "sine"
+        self.parameter_dict = {'k1': 0.2, 'k2': 0.06, 'omega': 0.007, 'duration': 2000}
+
+    def generate_input(self, parameter_dict):
+        parameter_dict = self.parameter_dict
+
+        input_sequence = {'left_wheel': [], 'right_wheel': []}
+        rospy.loginfo("[generate_input] generating input sequence of type {} with parameters {}".format("Sine", str(parameter_dict)))
+
+        for t in range(0,parameter_dict['duration'],10):
+            input_sequence['left_wheel'].append(parameter_dict['k1'] - parameter_dict['k2'] * cos(parameter_dict['omega'] * t))
+            input_sequence['right_wheel'].append(parameter_dict['k1'] + parameter_dict['k2'] * cos(parameter_dict['omega'] * t))
+        input_sequence['left_wheel'].append(0)
+        input_sequence['right_wheel'].append(0)
+
+        return input_sequence
+
+
+class ExperimentUI():
+
+    def __init__(self):
+        pass
+
+    def request_param_values(self, exp_name, param_dict):
+        param_names = param_dict.keys()
+
+        print "[UI INFO] Enter parameter values for {} experiment as prompted, if not entered default values are used\n".format(exp_name)
+        print "[UI INFO] parameters and their default values for {}: {}".format(exp_name, str(param_dict))
+
+        for param_name in param_names:
+            not_float_input = True
+            not_pressed_enter = True
+
+            while not_float_input and not_pressed_enter:
+                print "enter a value for {} ...".format(param_name)
+                user_input = raw_input()
+
+                try:
+                   valid_val = float(user_input) #try to cast into a float
+
+                   not_float_input = False
+                   param_dict[param_name] = user_input
+
+                   print("[UI INFO] input float number value is: ", user_input)
+                except ValueError:
+                    if user_input == "":
+                        print("[UI INFO] pressed enter -> using the default value for {}:{}".format(param_name,param_dict[param_name]))
+                        not_pressed_enter = False
+                    else:
+                        print("[UI WARNING] please enter a valid value (float or int)!")
+        print "[UI INFO] completed param_val acquisition"
+
+        return param_dict
+
+
+
+# Include basic utility functions here
--- catkin_ws/src/05-teleop/calibration/src/publish_control.py
+++ catkin_ws/src/05-teleop/calibration/src/publish_control.py
@@ -3,8 +3,7 @@ import rospy
 import rosnode
 from std_msgs.msg import String #Imports msg
 from duckietown_msgs.msg import WheelsCmdStamped, Twist2DStamped
-from math import sin, pi, cos
-import os
+
 from calibration.wheel_cmd_utils import *
 
 class Calibration:
@@ -15,32 +14,62 @@ class Calibration:
         # Create publisher
         publisher=rospy.get_param("~veh")+"/wheels_driver_node/wheels_cmd"
         self.pub_wheels_cmd = rospy.Publisher(publisher,WheelsCmdStamped,queue_size=1)
+
+        wait_for_rosbag = 5
+        rospy.loginfo("[Publish Control] starting the node and waiting {} seconds to ensure rosbag is recording".format(str(wait_for_rosbag)))
+        rospy.sleep(wait_for_rosbag)  # wait for the bag to start recording
+
+    def perform_experiments(self):
         DO_EXPERIMENT = "yes"
+        available_experiments = ["ramp_up", "sine"]
+        ui = ExperimentUI()
 
         while DO_EXPERIMENT == "yes":
-            print "Please, enter experiment name: "
+            print "Type the experiment type you want to do: {}".format(str(available_experiments))
             experiment_type = raw_input()
 
-            print "Do you want to make another experiment? [please respond with yes or no]"
+            if experiment_type == "ramp_up":
+                ramp_up = RampUp()
+                default_param_dict = ramp_up.parameter_dict
+                param_dict_request = ui.request_param_values(experiment_type, default_param_dict) # get user input to decide the set of params to be used.
+                print "[Calibration INFO] parameter set to be used in {} calibration: {}".format(experiment_type, str(param_dict_request))
+                wheel_cmds = ramp_up.generate_input(param_dict_request)
+                self.send_commands(wheel_cmds)
+            elif experiment_type == "sine":
+                sine = Sine()
+                default_param_dict = sine.parameter_dict
+                param_dict_request = ui.request_param_values(experiment_type, default_param_dict)
+                print "[Calibration INFO] parameter set to be used in {} calibration: {}".format(experiment_type, str(param_dict_request))
+                wheel_cmds = sine.generate_input(param_dict_request)
+                self.send_commands(wheel_cmds)
+            else:
+                print "NOT A VALID EXPERIMENT OPTION"
+
+            print "\n\nDo you want to do another experiment? [respond with yes or no]\n\n"
             user_wish = raw_input()
+
             DO_EXPERIMENT = user_wish.strip().lower()
-            print "See [{}]".format(DO_EXPERIMENT)
         else:
-            
-            print "thanks, no more experiments for now."
+            print "farewell, no more experiments for now."
 
+    def send_commands(self, wheel_cmds_dict):
+        vel_right_array = wheel_cmds_dict['right_wheel']
+        vel_left_array = wheel_cmds_dict['left_wheel']
 
-    def sendCommand(self, vel_right, vel_left):
         # Put the wheel commands in a message and publish
         msg_wheels_cmd = WheelsCmdStamped()
-        msg_wheels_cmd.header.stamp = rospy.Time.now()
-        msg_wheels_cmd.vel_right =vel_right
-        msg_wheels_cmd.vel_left =vel_left
-        self.pub_wheels_cmd.publish(msg_wheels_cmd)
 
-        rospy.sleep(1/self.frequency)
+        for i in range(len(vel_right_array)):
+            msg_wheels_cmd.header.stamp = rospy.Time.now()
+            msg_wheels_cmd.vel_right = vel_right_array[i]
+            msg_wheels_cmd.vel_left = vel_left_array[i]
+
+            rospy.loginfo("Left Wheel: {} \t Right Wheel: {}".format(vel_left_array[i], vel_right_array[i]))
+            self.pub_wheels_cmd.publish(msg_wheels_cmd)
+            rospy.sleep(1/self.frequency)
 
 if __name__ == '__main__':
-    rospy.sleep(5) #wait for the bag to start recording
-    calib=Calibration()
+    calib = Calibration()
+    calib.perform_experiments()
+
     #os.system("rosnode kill /record")
