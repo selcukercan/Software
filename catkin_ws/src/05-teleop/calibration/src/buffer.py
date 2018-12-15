b6030a6cf567e26d073b5300889d4b710a67445c
selcukercan <ercans@ethz.com>
2018-12-16 00:51:07 +0100

    end of the day

--- catkin_ws/src/05-teleop/calibration/src/buffer.py
+++ catkin_ws/src/05-teleop/calibration/src/buffer.py
@@ -11,66 +11,82 @@ from sensor_msgs.msg import CompressedImage
 from duckietown_msgs.msg import WheelsCmdStamped, Twist2DStamped, AprilTagDetectionArray
 from duckietown_msgs.msg import VehiclePoseEuler
 from calibration.wheel_cmd_utils import *
-# Service types to be imported from rosbag_recorder package
-from rosbag_recorder.srv import *
-from collections import deque
 
 class Buffer:
     def __init__(self):
         # namespace variables
         host_package = rospy.get_namespace()  # as defined by <group> in launch file
-        node_name = 'buffer_node'  # node name , as defined in launch file
-        host_package_node = host_package + node_name
+        self.node_name = 'buffer'  # node name , as defined in launch file
+        host_package_node = host_package + self.node_name
         self.veh = host_package.split('/')[1]
 
         # Initialize the node with rospy
         rospy.init_node('Buffer', anonymous=True)
 
         # Subscriber
-        sub_topic_image_process_status = '/' + self.veh + '/publish_detections_in_local_frame/image_requested' # 30 Hz
-        self.sub_proccesed_image = rospy.Subscriber(sub_topic_image_process_status, Bool, self.cb_recieved_ack, queue_size=1)
+        sub_topic_image_request = '/' + self.veh + '/publish_detections_in_local_frame/image_requested' # 30 Hz
+        self.sub_proccesed_image = rospy.Subscriber(sub_topic_image_request, Bool, self.cb_recieved_ack, queue_size=1)
         #self.recieved_pub_image_request = False
 
         # Publisher
         pub_topic_compressed_image= '/' + self.veh + "/buffer_node/image/compressed"
         self.pub_compressed_image = rospy.Publisher(pub_topic_compressed_image, CompressedImage, queue_size=1)
 
-        # Rosbag related parameters
-        path_to_bag = "/home/selcuk/input.bag"
-        self.cur_image_i = 0
+        # ROS Parameters
+        self.paramname = "/" + self.veh + "/buffer_node/process_status"
+        self.processing_status = self.setupParam(self.paramname,0)
 
+        # Rosbag related parameters
+        #path_to_bag = "/home/selcuk/input.bag"
+        path_to_bag = rospy.get_param(param_name= host_package_node + "/input_rosbag")
+        print path_to_bag + "*******************"
+        self.cur_image_i = 0
+        self.finished_processing = False
+        # Read the compressed image topic from the bag
         topicname = "/" + self.veh + "/camera_node/image/compressed"
         self.input_rosbag = rosbag.Bag(path_to_bag)
         self.total_msg_count = self.input_rosbag.get_message_count(topicname)
-        self.view_generator = self.input_rosbag.read_messages(topics=topicname) # create a rosbag generator object
+        self.view_generator = self.input_rosbag.read_messages(topics=topicname) # create a rosbag generator
 
-        self.pub_single_compressed_image() # send first image
-        self.cur_image_i += 1
+        init_wait = 3
+        rospy.loginfo("[{}] Waiting for {} seconds to ensure all other processes have luanched".format(self.node_name, init_wait))
+        rospy.sleep(init_wait)
 
+        self.pub_single_compressed_image() # send the first image
+
+
+    def setupParam(self,param_name,default_value):
+        value = rospy.get_param(param_name,default_value)
+        rospy.set_param(param_name,value) #Write to parameter server for transparancy
+        rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
+        return value
 
     def cb_recieved_ack(self, msg):
-        if msg.data == True:
-            print("done processing, send a new image")
+        if self.finished_processing == False:
+            print("recieved new image request, and have more to sent")
             self.pub_single_compressed_image()
         else:
-            print("waiting for image processing to complete...")
+            print("!!!!!!!!!! SHOULD NOT BE HERE 1")
 
     def pub_single_compressed_image(self):
         if self.cur_image_i < self.total_msg_count: # if not all the messages are sent yet.
-            view_output = next(self.view_generator) # view_output class : <class 'rosbag.bag.BagMessage'>, has return value (topic_name, msg, time_stamp)
+            print self.cur_image_i
+            view_output = next(self.view_generator) # view_output class : <class 'rosbag.bag.BagMessage'>, has return value (topic_name, msg, time_stamp of the message)
 
             # message generation
-            msg = CompressedImage()
-            msg.data = view_output[1]
-            msg.header.stamp = view_output[2]
-            msg.header.frame_id = self.cur_image_i # is this assignment a problem?
-
-            print "Publishing image with stamp: {}".format(msg.header.stamp)
+            msg = CompressedImage() # create a compressed image object to publish
+            msg = view_output.message # message content of the view is what we want to publish
+            rospy.loginfo("[{}] publishing image {}".format(self.node_name, self.cur_image_i))
             self.pub_compressed_image.publish(msg) # publish the message
             self.cur_image_i += 1
         else: # after sending all messages close the bag
-            rospy.loginfo('processed all the messages, now closing the bag.')
-            self.input_rosbag.close()
+            if self.finished_processing == False:
+                rospy.loginfo('[{}] processed all the messages, now closing the bag.'.format(self.node_name))
+                self.processing_status = rospy.set_param(self.paramname,1)
+                self.input_rosbag.close()
+                self.finished_processing = True
+            else:
+                print("!!!!!!!!!! SHOULD NOT BE HERE 2 ")
 
 if __name__ == '__main__':
     buffer = Buffer()
--- catkin_ws/src/05-teleop/pi_camera/src/image_rect_full_ratio.py
+++ catkin_ws/src/05-teleop/pi_camera/src/image_rect_full_ratio.py
@@ -21,6 +21,7 @@ from pi_camera.camera_info import get_camera_info_for_robot
 class ImgRectFullRatio(object):
     """docstring for ImgRectFullRatio"""
     def __init__(self):
+
         self.node_name = rospy.get_name()
         self.active = True
         self.bridge = CvBridge()
@@ -34,6 +35,7 @@ class ImgRectFullRatio(object):
         self.sub_cam_info = rospy.Subscriber("camera_info",CameraInfo,self.cbCamInfo,queue_size=1)
         self.sub_switch = rospy.Subscriber("~switch",BoolStamped, self.cbSwitch, queue_size=1)
 
+        self.ready_to_start_processing = False
     def cbSwitch(self,switch_msg):
         self.active = switch_msg.data
 
@@ -50,60 +52,71 @@ class ImgRectFullRatio(object):
             disable_old_homography(robot_name)
             homography_dummy = get_homography_default()
             self.gpg = GroundProjectionGeometry(self.cam_info, homography_dummy)
+            self.ready_to_start_processing = True
 
     def cbImg(self,msg):
-        if not self.active:
-            return
-        if self.cam_info is None:
-            return
-        if self.gpg is None:
-            return
+        rospy.sleep(0.05)
+        if self.ready_to_start_processing:
+            #print "[RECT] XXXXXXXXXXXX    1     XXXXXXXXXXXX"
+            if not self.active:
+                print "[RECT] XXXXXXXXXXXX    NOT ACTIVE     XXXXXXXXXXXX"
+                return
+            if self.cam_info is None:
+                print "[RECT] XXXXXXXXXXXX    cam_info None     XXXXXXXXXXXX"
+                return
+            if self.gpg is None:
+                print "[RECT] XXXXXXXXXXXX    gpg None     XXXXXXXXXXXX"
+                return
+            #print "[RECT] XXXXXXXXXXXX    2     XXXXXXXXXXXX"
 
-        cv_image = self.bridge.imgmsg_to_cv2(msg, "mono8")
+            cv_image = self.bridge.imgmsg_to_cv2(msg, "mono8")
 
-        # undistort the image
-        # Oriin ratio = 1.65, change to lower ratio to increase the speed of apriltags detection
-        new_matrix, result_img = self.gpg.rectify_full(cv_image, ratio=1.65)
+            # undistort the image
+            # Oriin ratio = 1.65, change to lower ratio to increase the speed of apriltags detection
+            new_matrix, result_img = self.gpg.rectify_full(cv_image, ratio=1.65)
 
-        # resulting image has a different size
-        # bring back the image to the old size, also crop a little from the outside
-        # code from: https://docs.opencv.org/3.4.0/da/d6e/tutorial_py_geometric_transformations.html
-        # Preferable interpolation methods are cv2.INTER_AREA for shrinking and cv2.INTER_CUBIC (slow) & cv2.INTER_LINEAR for zooming
+            # resulting image has a different size
+            # bring back the image to the old size, also crop a little from the outside
+            # code from: https://docs.opencv.org/3.4.0/da/d6e/tutorial_py_geometric_transformations.html
+            # Preferable interpolation methods are cv2.INTER_AREA for shrinking and cv2.INTER_CUBIC (slow) & cv2.INTER_LINEAR for zooming
 
-        #crop
-        # code is from here: https://stackoverflow.com/questions/15589517/how-to-crop-an-image-in-opencv-using-python
-        # crop the height from pixel 120 to increase the speed of apriltags detection
-        # cut_height = 120
-        # result_img = result_img[cut_height:result_img.shape[0], 0:result_img.shape[1]]
-        # new_matrix[0, 2] = new_matrix[0, 2]
-        # new_matrix[1, 2] = new_matrix[1, 2] - cut_height
+            #crop
+            # code is from here: https://stackoverflow.com/questions/15589517/how-to-crop-an-image-in-opencv-using-python
+            # crop the height from pixel 120 to increase the speed of apriltags detection
+            # cut_height = 120
+            # result_img = result_img[cut_height:result_img.shape[0], 0:result_img.shape[1]]
+            # new_matrix[0, 2] = new_matrix[0, 2]
+            # new_matrix[1, 2] = new_matrix[1, 2] - cut_height
 
-        # resize
-        # result_img = cv2.resize(result_img,(cv_image.shape[1], cv_image.shape[0]), interpolation = cv2.INTER_AREA)
-        # ratio = 1
-        # result_img = cv2.resize(result_img,(int(result_img.shape[1]*ratio), int(result_img.shape[0]*ratio)), interpolation = cv2.INTER_AREA)
+            # resize
+            # result_img = cv2.resize(result_img,(cv_image.shape[1], cv_image.shape[0]), interpolation = cv2.INTER_AREA)
+            # ratio = 1
+            # result_img = cv2.resize(result_img,(int(result_img.shape[1]*ratio), int(result_img.shape[0]*ratio)), interpolation = cv2.INTER_AREA)
+            #print "[RECT] XXXXXXXXXXXX    2     XXXXXXXXXXXX"
 
-        img_msg = self.bridge.cv2_to_imgmsg(result_img, "mono8")
-        #print "Old Image h,w =", cv_image.shape
-        img_msg.header.stamp = msg.header.stamp
-        img_msg.header.frame_id = msg.header.frame_id
+            img_msg = self.bridge.cv2_to_imgmsg(result_img, "mono8")
+            #print "Old Image h,w =", cv_image.shape
+            img_msg.header.stamp = msg.header.stamp
+            img_msg.header.frame_id = msg.header.frame_id
 
-        rect_cam_info = self.cam_info
-        rect_cam_info.header.stamp = img_msg.header.stamp
-        rect_cam_info.header.frame_id = img_msg.header.frame_id
-        rect_cam_info.height = result_img.shape[0]
-        rect_cam_info.width = result_img.shape[1]
+            rect_cam_info = self.cam_info
+            rect_cam_info.header.stamp = img_msg.header.stamp
+            rect_cam_info.header.frame_id = img_msg.header.frame_id
+            rect_cam_info.height = result_img.shape[0]
+            rect_cam_info.width = result_img.shape[1]
 
-        #add new camera info matrix to the camera info message
-        #print "new_matrix", new_matrix
-        new_K = tuple(new_matrix.reshape(-1).tolist()[0])
-        #print "new_K, ", new_K
-        rect_cam_info.K = new_K
+            #add new camera info matrix to the camera info message
+            #print "new_matrix", new_matrix
+            new_K = tuple(new_matrix.reshape(-1).tolist()[0])
+            #print "new_K, ", new_K
+            rect_cam_info.K = new_K
 
-        #print "Image h, w = ", rect_cam_info.height, rect_cam_info.width
-        self.pub_rect.publish(img_msg)
-        self.pub_cam_info.publish(rect_cam_info)
-
+            #print "Image h, w = ", rect_cam_info.height, rect_cam_info.width
+            self.pub_rect.publish(img_msg)
+            self.pub_cam_info.publish(rect_cam_info)
+            #print "[RECT] XXXXXXXXXXXX    2     XXXXXXXXXXXX"
+        else:
+            rospy.logwarn("[image_rect_full_ratio] image_rect_full_ratio is not ready to start processing ")
 
 if __name__ == '__main__':
     rospy.init_node('image_rect_full_ratio',anonymous=False)
