3d7a5e9ca9590fefbea2ba84defe325423dbc2bc
selcukercan <ercans@ethz.com>
2019-02-10 01:32:13 +0100

    added hampel loss + remove disable homograpy in image_rect_full_ratio

--- catkin_ws/src/05-teleop/calibration/include/calibration/metrics.py
+++ catkin_ws/src/05-teleop/calibration/include/calibration/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from utils import get_param_from_config_file
+from plotting_utils import simple_plot
 
 measurement_coordinate_frame = get_param_from_config_file("express_measurements_in")
 
@@ -15,4 +16,19 @@ def norm_rmse(x, x_sim):
         return np.mean(norm_mse_rho + norm_mse_yaw)
 
 def rmse(x, x_sim):
+    """ root mean square error"""
     return np.sqrt(np.mean(np.power((x - x_sim),2)))
+
+def hampel(x, x_sim, delta):
+    """ hampel's loss
+    delta may be interpreted as the precision requested.
+    """
+    loss = np.where(np.abs(x-x_sim) < delta , 0.5*((x-x_sim)**2), delta*np.abs(x - x_sim) - 0.5*(delta**2))
+    simple_plot(None,loss)
+    return np.sum(loss)
+
+if __name__ == '__main__':
+    x_sim = np.arange(-10,10,1)
+    x = np.zeros(x_sim.shape)
+    delta = 1
+    y = hampel(x, x_sim, delta)
