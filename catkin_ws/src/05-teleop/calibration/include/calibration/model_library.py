1275028946080a53947632bafeefd7626a075859
Selcuk Ercan <ercans@ethz.ch>
2019-03-13 17:45:39 +0100

    deleted unused kinematic models

--- catkin_ws/src/05-teleop/calibration/include/calibration/model_library.py
+++ catkin_ws/src/05-teleop/calibration/include/calibration/model_library.py
@@ -105,6 +105,8 @@ class DynamicsDrive(BaseModelClass):
         vx = (dr * cmd_right + dl * cmd_left) # m/s
         omega = (dr * cmd_right - dl * cmd_left) / L # rad/s
 
+        if self.measurement_coordinate_system == 'cartesian':
+            raise NotImplementedError
         elif self.measurement_coordinate_system == 'polar':
             # position states in relation to kinetic states
             rho_dot = vx # m/s
@@ -113,86 +115,6 @@ class DynamicsDrive(BaseModelClass):
             return [rho_dot, theta_dot]
 
 
-"""
-class KinematicDrive2(BaseModelClass):
-
-    def __init__(self, measurement_coordinate_system):
-        self.name = "kinematic_drive2"
-        self.param_ordered_list = ['c', 'tr', 'L'] # it is used to enforce an order (to avoid possible confusions) while importing params from YAML as bounds are imported from model always.
-        self.model_params = {'c': {'param_init_guess':1.0, 'param_bounds': (None, None)},
-                             'tr': {'param_init_guess':0.0, 'param_bounds': (None, None)},
-                             'L' : {'param_init_guess':0.055, 'param_bounds': (0.05, 0.06)}}
-
-        self.measurement_coordinate_system = measurement_coordinate_system
-        rospy.loginfo("\nusing model type: [{}]".format(self.name))
-
-    def model(self, t, x, u, p):
-        # input commands + model params
-        (cmd_right, cmd_left) = u
-        (c, tr, L) = p
-
-        # kinetic states through actuation
-        vx_pred = (c * (cmd_right + cmd_left) + tr * cmd_right) / 2.0
-        omega_pred = (c * (cmd_right - cmd_left) + tr * cmd_right) / (2.0 * L)
-
-        # position states in relation to kinetic states
-        yaw_pred = (omega_pred)
-        x_pred = (np.cos(yaw_pred) * vx_pred)
-        y_pred = (np.sin(yaw_pred) * vx_pred)
-
-        return [x_pred, y_pred, yaw_pred]
-
-class KinematicDrive3(BaseModelClass):
-
-    def __init__(self, measurement_coordinate_system):
-        self.name = "kinematic_drive3"
-        self.param_ordered_list = ['c', 'tr', 'tl', 'L'] # it is used to enforce an order (to avoid possible confusions) while importing params from YAML as bounds are imported from model always.
-        self.model_params = {'c': {'param_init_guess':1.0, 'param_bounds': (None, None)},
-                             'tr': {'param_init_guess':0.0, 'param_bounds': (None, None)},
-                             'tl': {'param_init_guess': 0.0, 'param_bounds': (None, None)},
-                             'L' : {'param_init_guess':0.055, 'param_bounds': (0.05, 0.06)}}
-
-        self.measurement_coordinate_system = measurement_coordinate_system
-        rospy.loginfo("\nusing model type: [{}]".format(self.name))
-
-    def model(self, t, x, u, p):
-        # input commands + model params
-        (cmd_right, cmd_left) = u
-        (c, tr, tl, L) = p
-
-        # kinetic states through actuation
-        vx_pred = (c * (cmd_right + cmd_left) + tr * cmd_right + tl * cmd_left) / 2.0
-        omega_pred = (c * (cmd_right - cmd_left) + tr * cmd_right - tl * cmd_left) / (2.0 * L)
-
-        # position states in relation to kinetic state
-        yaw_pred = (omega_pred)
-        x_pred = (np.cos(yaw_pred) * vx_pred)
-        y_pred = (np.sin(yaw_pred) * vx_pred)
-
-        return [x_pred, y_pred, yaw_pred]
-
-class Model4(BaseModelClass):
-
-    def __init__(self, measurement_coordinate_system):
-        self.name = "model1"
-        rospy.loginfo("\nusing model type: [{}]".format(self.name))
-
-    def model(self, t, x, u, p):
-        # input commands + model params
-        (cmd_right, cmd_left) = u
-        (c, cl, tr) = p
-
-        # kinetic states through actuation
-        vx_pred = c * (cmd_right + cmd_left) * 0.5 + tr * (cmd_right - cmd_left) * 0.5
-        omega_pred = cl * (cmd_right - cmd_left) * 0.5 + tr * (cmd_right + cmd_left) * 0.5
-
-        # position states in relation to kinetic states
-        yaw_pred = (omega_pred)
-        x_pred = (np.cos(yaw_pred) * vx_pred)
-        y_pred = (np.sin(yaw_pred) * vx_pred)
-
-        return [x_pred, y_pred, yaw_pred]
-"""
 # Include basic utility functions here
 
 # Motivation for introducing this fn:
@@ -202,12 +124,9 @@ def model_generator(model_name = None, measurement_coordinate_system = 'cartesia
         rospy.logwarn('[model_library] model is not initialized'.format(model_name))
     elif model_name == 'kinematic_drive':
         return KinematicDrive(measurement_coordinate_system)
-    elif model_name == 'kinematic_drive2':
-        return KinematicDrive2(measurement_coordinate_system)
-    elif model_name == 'kinematic_drive3':
-        return KinematicDrive3(measurement_coordinate_system)
     else:
         rospy.logwarn('[model_library] model name {} is not valid!'.format(model_name))
+
 
 def simulate_horizan(model_object, t, x0, u, p):
     """
@@ -291,6 +210,8 @@ def simulate(model_object, t, x, u, p):
         x_sim = a.copy()
 
     return x_sim
+
+
 def forwardEuler(model_object, dt, x_cur, u_cur, p_cur):
     if model_object.measurement_coordinate_system == 'cartesian':
         x_next = [0, 0, 0]
@@ -301,6 +222,7 @@ def forwardEuler(model_object, dt, x_cur, u_cur, p_cur):
     for i, s in enumerate(ds):
         x_next[i] = x_cur[i] + ds[i] * dt
     return x_next
+
 
 if __name__ == '__main__':
     from plotting_utils import plot_system
