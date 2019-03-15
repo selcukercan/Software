884f561f914660b9bc231633b5d78069793c0ee6
selcukercan <ercans@ethz.com>
2019-03-15 18:08:03 +0100

    fixed breaking in case of non-detected at

--- catkin_ws/src/20-indefinite-navigation/apriltags2_ros/apriltags2_ros/src/publish_detections_in_local_frame.py
+++ catkin_ws/src/20-indefinite-navigation/apriltags2_ros/apriltags2_ros/src/publish_detections_in_local_frame.py
@@ -33,8 +33,8 @@ class ToLocalPose:
 
         # Publisher
         # single tag
-        self.pub_topic_name = host_package_node + '/tag_detections_local_frame'
-        self.pub_detection_in_robot_frame = rospy.Publisher(self.pub_topic_name ,VehiclePoseEuler,queue_size=1)
+        #self.pub_topic_name = host_package_node + '/tag_detections_local_frame'
+        #self.pub_detection_in_robot_frame = rospy.Publisher(self.pub_topic_name ,VehiclePoseEuler,queue_size=1)
         # tag array
         self.pub_multiple_tag = host_package_node + '/tag_detections_array_local_frame'
         self.pub_detection_in_robot_frame_array = rospy.Publisher(self.pub_multiple_tag ,VehiclePoseEulerArray,queue_size=1)
@@ -43,7 +43,7 @@ class ToLocalPose:
         sub_topic_name =  '/' + self.veh + '/tag_detections'
         self.sub_img = rospy.Subscriber(sub_topic_name, AprilTagDetectionArray, self.cbDetection)
 
-        self.image_id = 0
+        self.image_id = 0 # keep the image counter
 
     def setupParam(self,param_name,default_value):
         value = rospy.get_param(param_name,default_value)
@@ -51,68 +51,10 @@ class ToLocalPose:
         rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
         return value
 
-    """
-    def cbDetection(self,msg):
-        if (len(msg.detections) > 0):  # non-emtpy detection message
-            #print msg
-            # unpack the position and orientation returned by apriltags2 ros
-            t_msg = msg.detections[0].pose.pose.pose.position
-            q_msg = msg.detections[0].pose.pose.pose.orientation
-
-            # convert the message content into a numpy array as robot_pose_in_world_frame requires so.
-            t = np.array([t_msg.x, t_msg.y, t_msg.z])
-            q = np.array([q_msg.x, q_msg.y, q_msg.z, q_msg.w])
-
-            # express relative rotation of the robot wrt the global frame.
-            world_R_veh, world_t_veh = vehTworld(q,t)
-            veh_feaXYZ_world = rotation_matrix_to_euler(world_R_veh)
-
-            # convert from numpy float to standart python float to be written into the message
-            world_t_veh =  world_t_veh.tolist()
-            veh_feaXYZ_world = veh_feaXYZ_world.tolist()
-
-            # form message to publish
-            veh_pose_euler_msg = VehiclePoseEuler()
-            veh_pose_euler_msg.header.stamp = rospy.Time.now()
-            # position
-            veh_pose_euler_msg.posx = world_t_veh[0]
-            veh_pose_euler_msg.posy = world_t_veh[1]
-            veh_pose_euler_msg.posz = world_t_veh[2]
-            # orientation
-            veh_pose_euler_msg.rotx = veh_feaXYZ_world[0]
-            veh_pose_euler_msg.roty = veh_feaXYZ_world[1]
-            veh_pose_euler_msg.rotz = veh_feaXYZ_world[2]
-
-            # finally publish the message
-            self.pub_detection_in_robot_frame.publish(veh_pose_euler_msg)
-            self.image_id += 1
-            rospy.loginfo('publish image {} posx: {} posy:  {} rotz: {}'.format(self.image_id,veh_pose_euler_msg.posx,veh_pose_euler_msg.posy,veh_pose_euler_msg.rotz))
-
-        else:
-            rospy.loginfo("[{}] empty apriltag detection recieved".format(self.node_name))
-
-            if self.synchronous_mode:
-                # form message to publish
-                veh_pose_euler_msg = VehiclePoseEuler()
-                veh_pose_euler_msg.header.stamp = rospy.Time.now()
-                # position
-                veh_pose_euler_msg.posx = 0.
-                veh_pose_euler_msg.posy = 0.
-                veh_pose_euler_msg.posz = 0.
-                # orientation
-                veh_pose_euler_msg.rotx = 0.
-                veh_pose_euler_msg.roty = 0.
-                veh_pose_euler_msg.rotz = 0.
-
-                # finally publish the message
-                self.pub_detection_in_robot_frame.publish(veh_pose_euler_msg)
-                self.image_id += 1
-                rospy.loginfo("[{}] in synchronous mode publishing VehiclePoseEuler with entries equal to 0.0".format(self.node_name,self.image_id))
-    """
-
     def cbDetection(self, msg):
+        veh_pose_euler_array_msg = VehiclePoseEulerArray()
+        self.image_id += 1
         if (len(msg.detections) > 0):  # non-emtpy detection message
-            veh_pose_euler_array_msg = VehiclePoseEulerArray()
             veh_pose_euler_array_msg.local_pose_list = []
 
             for i in range(len(msg.detections)):
@@ -151,17 +93,15 @@ class ToLocalPose:
                 # id of the Apriltag
                 veh_pose_euler_msg.id = tag_id_msg
 
-                # finally publish the message
-                self.pub_detection_in_robot_frame.publish(veh_pose_euler_msg)
-                """
-                rospy.loginfo('\ntag id: {} publish posx: {} posy:  {} rotz: {}\n'.format(veh_pose_euler_msg.id,
-                                                                                          veh_pose_euler_msg.posx,
-                                                                                          veh_pose_euler_msg.posy,
-                                                                                          veh_pose_euler_msg.rotz))
-                """
                 veh_pose_euler_array_msg.local_pose_list.append(veh_pose_euler_msg)
-            self.pub_detection_in_robot_frame_array.publish(veh_pose_euler_array_msg)
-            #rospy.loginfo('detection array: {}'.format(veh_pose_euler_array_msg))
+
+            veh_pose_euler_array_msg.at_detected = True
+        else:
+            rospy.loginfo("[{}] no apriltags detected in image {}".format(self.node_name, self.image_id))
+            veh_pose_euler_array_msg.at_detected = False
+        # publish the at array message
+        self.pub_detection_in_robot_frame_array.publish(veh_pose_euler_array_msg)
+
 
 if __name__ == '__main__':
     to_local_pose = ToLocalPose()
