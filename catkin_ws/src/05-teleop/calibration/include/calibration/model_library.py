446d42e0f04c72c552b65edf68f1a1eb87b9b325
Selcuk Ercan <ercans@ethz.ch>
2019-04-10 20:10:22 +0200

    added InputDependentKinematicDrive class

--- catkin_ws/src/05-teleop/calibration/include/calibration/model_library.py
+++ catkin_ws/src/05-teleop/calibration/include/calibration/model_library.py
@@ -125,6 +125,126 @@ class KinematicDrive(BaseModelClass):
         return x_sim
 
 
+class InputDependentKinematicDrive(BaseModelClass):
+
+    def __init__(self, interval_count=1):
+        self.name = "input_dependent_kinematic_drive"
+        rospy.loginfo("\nusing model type: [{}]".format(self.name))
+
+        # Note that we assume that the vehicle is movinf forward, hence d in [0,1].
+        self.intervals = np.linspace(0, 1, interval_count + 1) # for interval_count = 1 returns array([0., 1.])
+        self.param_ordered_list = self.generate_param_ordered_list()
+        self.model_params = self.generate_model_params(self.param_ordered_list)
+
+    def model(self, t, x, u, p):
+        # input commands + model params
+        (cmd_right, cmd_left) = u
+        #(dr, dl, L) = p
+
+        # extract the correct parameter depending on the input
+        dr, dl, L = self.input_dependent_parameter_selector(u,p)
+
+        # kinetic states through actuation
+        vx = (dr * cmd_right + dl * cmd_left)  # m/s
+        omega = (dr * cmd_right - dl * cmd_left) / L  # rad/s
+
+        # position states in relation to kinetic states
+        rho_dot = vx  # m/s
+        theta_dot = omega  # rad/s
+
+        return [rho_dot, theta_dot]
+
+    def simulate(self, t, x, u, p):
+        """
+        Note that this function performs N step ahead propagation of the initial state
+        for in one step ahead manner
+        Args:
+            model_object: a model object as defined by model library.
+            t (list) : time array for which the predictions will be made.
+            x (list) : measured states; x, y, yaw
+            u (numpy.ndarray): 2*n array, whose first row is wheel_right_exec, and second row is wheel_left_exec. n is the number of time-steps.
+            p (list): model parameters.
+        Returns:
+            x_sim (numpy.ndarray): 3*n array, containing history of state evolution.
+        """
+        x0 = x[:, 0]
+        x_sim = reshape_x(x0)
+
+        for i in range(len(t) - 1):
+            t_cur, t_next = t[
+                            i:i + 2]  # prediction will be made in between two consecutive time steps, note that this does not require fixed time step.
+            x0 = x[:, i]
+            # one-step-ahead prediction
+            sol = forward_euler_vel_to_pos(self.model, (t_next - t_cur), x0, u[:, i], p)
+            a = np.hstack([x_sim, reshape_x(sol)])
+            x_sim = a.copy()
+        return x_sim
+
+    def simulate_horizan(self, t, x0, u, p):
+        """
+        Note that this function performs N step ahead propagation of the initial state
+        for given input sequence u.
+        Args:
+            model_object: a model object as defined by model library.
+            t (list) : time array for which the predictions will be made.
+            x0 (list) : initial values of the states; x, y, yaw
+            u (numpy.ndarray): 2*n array, whose first row is wheel_right_exec, and second row is wheel_left_exec. n is the number of time-steps.
+            p (list): model parameters.
+        Returns:
+            x_sim (numpy.ndarray): 3*n array, containing history of state evolution.
+        """
+        x_sim = reshape_x(x0)
+        for i in range(len(t) - 1):
+            t_cur, t_next = t[
+                            i:i + 2]  # prediction will be made in between two consecutive time steps, note that this does not require fixed time step.
+            sol = forward_euler_vel_to_pos(self.model, (t_next - t_cur), x0, u[:, i], p)
+            a = np.hstack([x_sim, reshape_x(sol)])
+            x_sim = a.copy()
+            x0 = sol
+        return x_sim
+
+    def generate_param_ordered_list(self):
+        param_ordered_list = []
+        for i in range(len(self.intervals)-1):
+            param_ordered_list.extend(["dr_" + str(i), "dl_" + str(i)])
+        param_ordered_list.append("L")
+        return param_ordered_list
+
+    def generate_model_params(self, param_ordered_list):
+        model_params = {}
+        for i in np.arange(len(param_ordered_list)-1):
+            model_params[param_ordered_list[i]] = {'param_init_guess': 0.85, 'param_bounds': (None, None)}
+        model_params["L"] = {'param_init_guess': 0.055, 'param_bounds': (0.05, 0.06)}
+        return model_params
+
+    def input_dependent_parameter_selector(self, u, p):
+        (cmd_right, cmd_left) = u
+
+        # right drive constant
+        bin_id_right = np.searchsorted(self.intervals, cmd_right)
+        index_right = 2 * (bin_id_right-1)
+        param_right_name = self.param_ordered_list[index_right]
+        param_right_val = p[index_right]
+
+        # left drive constant
+        bin_id_left = np.searchsorted(self.intervals, cmd_left)
+        index_left = 2 * (bin_id_right-1) + 1
+        param_left_name = self.param_ordered_list[index_left]
+        param_left_val = p[index_left]
+        """
+        print("intervals: {}".format(self.intervals))
+        print(self.model_params)
+        print("intervals: {}".format(self.intervals))
+        print("p: {}".format(p))
+        print("param_ordered_list: {}".format(self.param_ordered_list))
+        
+        print("cmd_right: {} \t index_right: {} ".format(cmd_right, index_right))
+        print("Param Right Name: {} \t Param Right Value: {} ".format(param_right_name, param_right_val))
+        print("cmd_left: {} \t index_left: {} ".format(cmd_left, index_left))
+        print("Param Left Name: {} \t Param Left Value: {} ".format(param_left_name, param_left_val))
+        """
+        return param_right_val, param_left_val, p[-1]
+
 class DynamicDrive(BaseModelClass):
 
     def __init__(self):
@@ -269,6 +389,8 @@ def model_generator(model_name=None):
         return KinematicDrive()
     elif model_name == 'dynamic_drive':
         return DynamicDrive()
+    elif model_name == "input_dependent_kinematic_model":
+        return InputDependentKinematicDrive()
     else:
         rospy.logwarn('[model_library] model name {} is not valid!'.format(model_name))
 
@@ -295,25 +417,12 @@ def forward_euler_vel_to_pos(model, dt, x_cur, u_cur, p_cur):
 
 
 if __name__ == '__main__':
-    from plotting_utils import multiplot
-    from calibration.utils import work_space_settings
-
-    work_space_settings()
-    # Testing model and simulate functions
-    dd = model_generator('dynamic_drive', 'polar')
-
-    t_beg = 0
-    t_end = 4
-    t_step = 1
-
-    t = np.arange(t_beg, t_end, t_step)
-
-    x_dot = np.array([[0, 1, 1, 1], [0, 0, 0, 1]])
-    u = np.vstack([np.ones(np.size(t)) * 1.0, np.ones(np.size(t)) * 1.0])
-    p = [0.5, 0, 0, 0.5, 0, 0, 1, 1, 1, 1]
-
-    x_sim = dd.simulate(t, x_dot, u, p)
-
-    multiplot(states_list=[x_sim], time_list=[t], experiment_name_list=["simulation"], plot_title='dynamics',
-              save=False)
+    t = 0
+    x = 0
+    u_r = 1
+    u_l = 1
+    u = (u_r, u_l)
+    p = [1, 1, 1, 1, 1, 1, 1, 1, 0.5]
+    ikd = InputDependentKinematicDrive(interval_count=4)
+    ikd.model(t,x,u,p)
     print("selcuk")
