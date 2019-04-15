6c9f02a468a13d6097d72a40aa0e110a9cf3ac01
Selcuk Ercan <ercans@ethz.ch>
2019-04-15 21:25:35 +0200

    post-processing works end-to-end

--- catkin_ws/src/10-lane-control/ground_projection/include/ground_projection/ground_projection_geometry.py
+++ catkin_ws/src/10-lane-control/ground_projection/include/ground_projection/ground_projection_geometry.py
@@ -182,10 +182,10 @@ class GroundProjectionGeometry(object):
         '''
         W = int(self.pcm.width * ratio)
         H = int(self.pcm.height * ratio)
-#        mapx = np.ndarray(shape=(H, W, 1), dtype='float32')
-#        mapy = np.ndarray(shape=(H, W, 1), dtype='float32')
-        print('K: %s' % self.pcm.K)
-        print('P: %s' % self.pcm.P)
+#       mapx = np.ndarray(shape=(H, W, 1), dtype='float32')
+#       mapy = np.ndarray(shape=(H, W, 1), dtype='float32')
+        #print('K: %s' % self.pcm.K)
+        #print('P: %s' % self.pcm.P)
 
 #        alpha = 1
 #        new_camera_matrix, validPixROI = cv2.getOptimalNewCameraMatrix(self.pcm.K, self.pcm.D, (H, W), alpha)
@@ -195,7 +195,7 @@ class GroundProjectionGeometry(object):
         new_camera_matrix = self.pcm.K.copy()
         new_camera_matrix[0, 2] = W / 2
         new_camera_matrix[1, 2] = H / 2
-        print('new_camera_matrix: %s' % new_camera_matrix)
+        #print('new_camera_matrix: %s' % new_camera_matrix)
         mapx, mapy = cv2.initUndistortRectifyMap(self.pcm.K, self.pcm.D, self.pcm.R,
                                                  new_camera_matrix, (W, H),
                                                  cv2.CV_32FC1)
